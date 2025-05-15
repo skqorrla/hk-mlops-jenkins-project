e30019bb73d98e5de58d2f039f0c4b128851d5ff
skqorrla <beomseo00@gmail.com>
2025-05-15 10:37:51 +0900

    groovy change

--- script.groovy
+++ script.groovy
@@ -1,12 +1,12 @@
-def BuildApp() {
+def buildApp() {
     echo '빌드하는 중~!~!~!'
 }
 
-def TestApp() {
+def testApp() {
     echo '테스트 하는 중 ~~'
 }
 
-def DeployApp() {
+def deployApp() {
     echo '배포 하는 중 ~~'
     echo "배포 버전 : ${params.VERSION}"
 }
